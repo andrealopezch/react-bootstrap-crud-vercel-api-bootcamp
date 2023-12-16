8ce9cba8be8a422dcec98ff2198d0aaf1919ba6c
Andrea  López Chico <andrealopezchico@Andreas-MacBook-Pro.local>
2023-12-16 12:10:20 -0600

    Actualizacion del componente Estudiantes 1.2

--- src/components/Estudiantes.jsx
+++ src/components/Estudiantes.jsx
@@ -12,6 +12,28 @@ function Estudiantes() {
       });
   }, []);
 
+  const saveStudent = () => {
+    const nuevoEstudiante = {
+      id: 108,
+      name: "Andrea",
+      lastname: "Lopez",
+      notes: "bien",
+    };
+
+    fetch(
+      "https://demobootcamp-vercel-api-node-postgress.vercel.app/students",
+      {
+        method: "POST",
+        body: JSON.stringify(nuevoEstudiante),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {});
+  };
+
   return (
     <>
       <h2>Lista de Estudiantes</h2>
@@ -20,9 +42,11 @@ function Estudiantes() {
         dataestudiantes.map((infoestudiantes, index) => (
           <div key={index}>
             {infoestudiantes.name}
+            &nbsp;
             {infoestudiantes.lastname}
           </div>
         ))}
+      <button onClick={saveStudent}>Guardar</button>
     </>
   );
 }
